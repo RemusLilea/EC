0b02292928a7913e60ae6afb35580c8192b0af73
RemusLilea <126488214+RemusLilea@users.noreply.github.com>
2023-03-10 18:21:56 +0200

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+
+# Update the package index
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker.io
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
+
+# Check that Docker is installed correctly
+sudo docker run hello-world
